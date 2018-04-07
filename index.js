d14bb975231271763e780a1e4f3d9a2599d1f028
md Enam <russell.m2006@gmail.com>
2018-04-07 01:55:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,46 @@
+function shout(string) {
+  return ('hello').toUpperCase();
+}
+
+function whisper(string){
+  return ('HELLO').toLowerCase();
+}
+
+function logshout(string){
+  console.log(string.toUpperCase());
+}
+  
+  
+
+function logwhisper(string){
+
+   console.log(string.toLowerCase());
+}
+
+logwhisper('HELLO');
+
+
+function sayHiToGrandma(string){
+  var str = 'I ';
+  return( str)+('CAN\'T HEAR YOU!').toLowerCase();
+
+}
+
+function sayHiToGrandma(string){
+  
+ return('yes indeed!').toUpperCase();
+  
+
+
+
+
+function sayHiToGrandma(string){
+  var sayHi = "I love you, grandma"
+  var grandma = "I love you, too."
+ return (grandma)
+ }
+} 
+
+
+
+
