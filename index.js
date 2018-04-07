3cac6a6bf22905396ff4f4005fdb38da608595e4
md Enam <russell.m2006@gmail.com>
2018-04-07 02:19:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,18 @@ function sayHiToGrandma(string){
 // 2. compare that to 'string'
 
 if (string.toLowerCase() === string){
-  return 
+  returfunction sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }n 
 }
 
 
